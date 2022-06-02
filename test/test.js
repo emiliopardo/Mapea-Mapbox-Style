f8df7de83436bc6d3e22e08865ba68950da5cf5f
emiliopardo <e.pardo.perez@gmail.com>
2022-06-02 07:36:23 +0200

    se actualizan ejemplos test y prod

--- test/test.js
+++ test/test.js
@@ -1,5 +1,36 @@
 import { MapboxStyle } from 'facade/index.js';
 
+const osm = new M.layer.OSM();
+
+const ortofoto2016_color = new M.layer.WMS({
+  url: 'https://ideandalucia.es/wms/ortofoto2016?',
+  name: 'ortofotografia_2016_rgb',
+  legend: 'Ortofoto 2016 Color ',
+  version: '1.1.1',
+  transparent: false,
+  tiled: true
+});
+
+const ortofoto2016_pancromatica = new M.layer.WMS({
+  url: 'https://ideandalucia.es/wms/ortofoto2016?',
+  name: 'ortofotografia_2016_pancromatico',
+  legend: 'Ortofoto 2016 Escala de Grises',
+  version: '1.1.1',
+  transparent: false,
+  tiled: true
+});
+
+const fondo_blanco = new M.layer.WMS({
+  url: 'https://www.ideandalucia.es/services/andalucia/wms?',
+  name: 'andalucia:btn100_0101s_uni_admin_paises',
+  legend: 'Sin Fondo',
+  version: '1.1.1',
+  transparent: false,
+  tiled: true
+})
+
+fondo_blanco.setOpacity(0)
+
 const mapjs = M.map({
   container: 'map',
   projection: 'EPSG:3857*m',
@@ -9,21 +40,14 @@ const mapjs = M.map({
     draw: false
   },
   controls: ['layerswitcher', 'panzoombar', 'mouse'],
-  layers: [new M.layer.OSM(),new M.layer.WMS({
-      url: 'https://ideandalucia.es/wms/ortofoto2016?',
-      name: 'ortofotografia_2016_rgb',
-      legend: 'Ortofoto 2016',
-      version: '1.1.1',
-      transparent: false,
-      tiled: true
-    })],
+  layers: [osm,ortofoto2016_color,ortofoto2016_pancromatica,fondo_blanco],
   zoom: 6
 });
 
 
 
 // Capa MVT
-let mapa_andalucia_color = new MapboxStyle({
+const mapa_andalucia_color = new MapboxStyle({
   url: 'https://ws205.juntadeandalucia.es/tileserver/data/mapa_andalucia/{z}/{x}/{y}.pbf',
   style: 'https://ws205.juntadeandalucia.es/tileserver/styles/mapa_andalucia/style.json',
   name: 'Mapa de Andalucia',
@@ -31,7 +55,7 @@ let mapa_andalucia_color = new MapboxStyle({
   projection: 'EPSG:3857',
 });
 
-let mapa_andalucia_fondo_negro = new MapboxStyle({
+const mapa_andalucia_fondo_negro = new MapboxStyle({
   url: 'https://ws205.juntadeandalucia.es/tileserver/data/mapa_andalucia/{z}/{x}/{y}.pbf',
   style: 'https://factorylab02.ieca.junta-andalucia.es/tileserver/styles/mapa_andalucia_gray_scale_fondo_negro/style.json',
   name: 'Mapa de Andalucia fondo negro',
@@ -40,20 +64,27 @@ let mapa_andalucia_fondo_negro = new MapboxStyle({
 });
 
 
-let osm_andalucia_color = new MapboxStyle({
-  url: 'https://factorylab02.ieca.junta-andalucia.es/tileserver/data/mapa_andalucia_osm/{z}/{x}/{y}.pbf',
-  style: 'https://factorylab02.ieca.junta-andalucia.es/tileserver/styles/style-osm-bright/style.json',
+const osm_andalucia_color = new MapboxStyle({
+  url: 'https://ws205.juntadeandalucia.es/tileserver/data/mapa_andalucia_osm/{z}/{x}/{y}.pbf',
+  style: 'https://ws205.juntadeandalucia.es/tileserver/styles/style-osm-bright/style.json',
   name: 'Open Street Maps',
   sourceName: 'mapa_andalucia_osm',
   projection: 'EPSG:3857',
 });
 
 mapjs.addLayers([mapa_andalucia_color,osm_andalucia_color,mapa_andalucia_fondo_negro]);
-// mapjs.addLayers([mapa_andalucia_color]);
-// mapjs.addLayers([mapa_andalucia_fondo_negro]);
-// mapa_andalucia_fondo_negro.transparent = false;
-// osm_andalucia_color.transparent = false;
-//  osm_andalucia_color.transparent = false;
-// mapjs.getLayers()[0].setOpacity(0);
 
-console.log(mapjs.getLayers())
+// const mapjs = M.map({
+//   container: 'map',
+//   projection: 'EPSG:3857*m',
+//   center: {
+//     x: -575849.044,
+//     y: 4497865.157,
+//     draw: false
+//   },
+//   controls: ['layerswitcher', 'panzoombar', 'mouse'],
+//   layers: [[mapa_andalucia_color,osm_andalucia_color,mapa_andalucia_fondo_negro]],
+//   zoom: 6
+// });
+
+// console.log(mapjs.getLayers())
